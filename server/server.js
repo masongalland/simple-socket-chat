825eb4281992534cc34107ababf9016c2ac19ba2
masongalland <masongalland@me.com>
2018-10-02 12:30:21 -0600

    changed the port the server runs on and removed refs.

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
-const express = require('express')
-    , socket = require('socket.io');
+const express = require('express'),
+  socket = require('socket.io');
 
-const app = express()
-    , io = socket(app.listen(4000, () => console.log('Server listening on port 4000')));
-
+const app = express(),
+  io = socket(app.listen(3005, () => console.log('Server listening on port 3005')));
 
 io.on('connection', socket => {
   console.log('User Connected');
-  socket.emit("welcome", {userID: socket.id})
+  socket.emit('welcome', { userID: socket.id });
 
   socket.on('join room', data => {
-    console.log('Room joined', data.room)
+    console.log('Room joined', data.room);
     socket.join(data.room);
     io.to(data.room).emit('room joined', data.room);
-  })
+  });
 
   socket.on('message sent', function(data) {
-    data.user = this.id
+    data.user = this.id;
     io.to(data.room).emit('message dispatched', data);
-  })
+  });
 
   socket.on('disconnect', () => {
     console.log('User Disconnected');
-  })
+  });
 });
--- src/App.js
+++ src/App.js
@@ -7,77 +7,84 @@ class App extends Component {
     super();
     this.state = {
       userID: null,
-      messages: []
-    }
-
-    this.updateMessages = this.updateMessages.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
-    this.setUserId = this.setUserId.bind(this);
-    this.joinRoom = this.joinRoom.bind(this);
-    this.joinSuccess = this.joinSuccess.bind(this);
+      messages: [],
+      userInput: '',
+      room: '1',
+    };
   }
 
   componentDidMount() {
     this.socket = io('/');
-    this.socket.on('message dispatched', this.updateMessages)
-    this.socket.on('welcome', this.setUserId)
-    this.socket.on('room joined', this.joinSuccess)
-    this.joinRoom()
+    this.socket.on('message dispatched', this.updateMessages);
+    this.socket.on('welcome', this.setUserId);
+    this.socket.on('room joined', this.joinSuccess);
+    this.joinRoom('1');
   }
 
-  updateMessages(message) {
-    const updatedMessages = this.state.messages.slice()
-    updatedMessages.push(message)
+  handleInput = e => {
+    this.setState({ userInput: e.target.value });
+  };
+
+  handleEnter = e => {
+    if (e.key !== 'Enter') return;
+    this.sendMessage();
+  };
+
+  updateMessages = message => {
+    const updatedMessages = this.state.messages.slice();
+    updatedMessages.push(message);
     this.setState({
-      messages: updatedMessages
-    })
-  }
+      messages: updatedMessages,
+    });
+  };
 
-  setUserId(user) {
-    this.setState(user)
-  }
+  setUserId = user => {
+    this.setState(user);
+  };
 
-  sendMessage() {
+  sendMessage = () => {
     this.socket.emit('message sent', {
-      message: this.refs.message.value,
-      room: this.refs.room.value
-    })
-    this.refs.message.value = '';
-  }
+      message: this.state.userInput,
+      room: this.state.room,
+    });
+    this.setState({ userInput: '' });
+  };
 
-  joinRoom() {
-    this.socket.emit('join room', {
-      room: this.refs.room.value
-    })
-    this.setState({messages: []})
-  }
+  joinRoom = room => {
+    this.socket.emit('join room', { room });
+    this.setState({ messages: [], room });
+  };
 
   joinSuccess(room) {
-    console.log("you successfully joined room " + room)
+    console.log('you successfully joined room ' + room);
   }
 
   render() {
-    const messages = this.state.messages.map((e,i) => {
-      const styles = e.user === this.state.userID ? {alignSelf: "flex-end", backgroundColor: "#2d96fb", color: "white"} : {alignSelf: "flex-start", backgroundColor: "#e5e6ea"}
+    const { messages, userInput, userID, room } = this.state;
+    const messagesToDisplay = messages.map((e, i) => {
+      const styles =
+        e.user === userID
+          ? { alignSelf: 'flex-end', backgroundColor: '#2d96fb', color: 'white' }
+          : { alignSelf: 'flex-start', backgroundColor: '#e5e6ea' };
       return (
-        <p key={i} style={styles}>{e.message}</p>
-      )
-    })
+        <p key={i} style={styles}>
+          {e.message}
+        </p>
+      );
+    });
 
     return (
       <div className="App">
         <header>
           <h3>select a room: </h3>
-          <select ref="room" defaultValue="1" onChange={this.joinRoom}>
+          <select value={room} onChange={e => this.joinRoom(e.target.value)}>
             <option> 1 </option>
             <option> 2 </option>
           </select>
         </header>
-        <div className="messages">
-          {messages}
-        </div>
+        <div className="messages">{messagesToDisplay}</div>
         <div className="input">
-          <input ref="message" />
+          <input value={userInput} onChange={this.handleInput} onKeyPress={this.handleEnter} />
           <button onClick={this.sendMessage}>Send</button>
         </div>
       </div>
